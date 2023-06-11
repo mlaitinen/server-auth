df008049cbdefe12bfa13f6dbcfef2da6aaa38de
Miku Laitinen <miku.laitinen@web-veistamo.fi>
2023-06-11 13:32:45 +0300

    [MIG] auth_oauth_groups and auth_oauth_groups_microsoft from 13 to 16

--- auth_oauth_groups/__manifest__.py
+++ auth_oauth_groups/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "OAuth Group Mapping",
-    "version": "13.0.1.0.1",
+    "version": "16.0.1.0.0",
     "website": "https://github.com/OCA/server-auth",
     "depends": [
         "auth_oauth",
@@ -14,6 +14,6 @@
     "data": [
         "views/auth_oauth_group_views.xml",
         "views/auth_oauth_provider_views.xml",
-        "security/ir.model.access.csv"
+        "security/ir.model.access.csv",
     ],
 }
